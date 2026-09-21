e006980b05c1210d768a35ce77f62b1f2f77256c
karnaykhovilyaaress-cmyk <karnaykhovilyaaress@gmail.com>
2026-09-21 23:20:24 +0800

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,19 @@
+import asyncio
+import os
+from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+TOKEN = os.environ.get("BOT_TOKEN")
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def start(message: Message):
+    await message.answer(f"Привет, {message.from_user.full_name}! Я бот ИРНИТУ.")
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
